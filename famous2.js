0703d3f9f6bb41d5bc94f47fd99a926c55c074a3
hajikhan <hajikhan110@yahoo.com>
2024-02-23 15:08:31 +0500

    msg

---
+++ famous2.js
@@ -0,0 +1,5 @@
+var author = 'William Shakespeare';
+var fmessage = 'A rose by any other name would smell as sweet. ';
+//console.log('${famous}');
+console.log("Authour : ".concat(author, " , said"));
+console.log("\" ".concat(fmessage, "\""));
---
+++ famous2.ts
@@ -0,0 +1,7 @@
+let author :string = 'William Shakespeare';
+
+let fmessage :string ='A rose by any other name would smell as sweet. ';
+//console.log('${famous}');
+console.log(`Authour : ${author} , said`);
+console.log(`" ${fmessage}"`);
+
